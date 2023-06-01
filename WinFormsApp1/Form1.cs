a28c08d801718777d11e0a5b09eef674b865c7cc
Tzeno <nicktznlr2@gmail.com>
2023-06-01 16:55:46 +0300

    v0.1

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
@@ -51,7 +46,7 @@ namespace WinFormsApp1
         OpponentTeamImages[i].Text = OpponentTeam[i].Name;
       }
 
-      
+
 
 
 
